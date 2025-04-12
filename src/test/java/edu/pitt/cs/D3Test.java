a70fb792843a5e011ae45d85910f2bcc678deb05
2004Moonlove <1079019149y@gmail.com>
2025-04-11 21:05:29 -0400

    code

--- src/test/java/edu/pitt/cs/D3Test.java
+++ src/test/java/edu/pitt/cs/D3Test.java
@@ -25,44 +25,49 @@ import org.openqa.selenium.Keys;
 import java.util.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+
 public class D3Test {
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
-  //
-  ChromeOptions options = new ChromeOptions();
-  options.addArguments("--headless");
-  driver = new ChromeDriver(options);
-  //
+
   @Before
   public void setUp() {
+    //
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    driver = new ChromeDriver(options);
+    //
     driver = new FirefoxDriver();
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
   }
+
   @After
   public void tearDown() {
     driver.quit();
   }
+
   @Test
   public void tEST1LINKS() {
     // Test name: TEST-1-LINKS
     // Step # | name | target | value
-    // 1 | open | / | 
+    // 1 | open | / |
     driver.get("http://localhost:8080//");
-    // 2 | verifyElementPresent | xpath=//a[@href='/reset' and text()='Reset'] | 
+    // 2 | verifyElementPresent | xpath=//a[@href='/reset' and text()='Reset'] |
     {
       List<WebElement> elements = driver.findElements(By.xpath("//a[@href=\'/reset\' and text()=\'Reset\']"));
-      assert(elements.size() > 0);
+      assert (elements.size() > 0);
     }
   }
+
   @Test
   public void tEST2RESET() {
     // Test name: TEST-2-RESET
     // Step # | name | target | value
-    // 1 | open | https://cs1632.appspot.com/ | 
+    // 1 | open | https://cs1632.appspot.com/ |
     driver.get("http://localhost:8080/");
-    // 2 | click | link=Reset | 
+    // 2 | click | link=Reset |
     driver.findElement(By.linkText("Reset")).click();
     // 3 | assertText | xpath=//div[@id='listing']/ul/li | ID 1. Jennyanydots
     assertThat(driver.findElement(By.xpath("//div[@id=\'listing\']/ul/li")).getText(), is("ID 1. Jennyanydots"));
@@ -71,47 +76,51 @@ public class D3Test {
     // 5 | assertText | xpath=//div[@id='listing']/ul/li[3] | ID 3. Mistoffelees
     assertThat(driver.findElement(By.xpath("//div[@id=\'listing\']/ul/li[3]")).getText(), is("ID 3. Mistoffelees"));
   }
-@Test
+
+  @Test
   public void tEST3CATALOG() {
     driver.get("http://localhost:8080//");
     driver.findElement(By.linkText("Catalog")).click();
     {
       List<WebElement> elements = driver.findElements(By.xpath("//ol/li[1]/img[@src=\'/images/cat1.jpg\']"));
-      assert(elements.size() > 0);
+      assert (elements.size() > 0);
     }
   }
+
   @Test
   public void tEST5RENTACAT() {
     // Test name: TEST-5-RENT-A-CAT
     // Step # | name | target | value
-    // 1 | open | /rent-a-cat | 
+    // 1 | open | /rent-a-cat |
     driver.get("http://localhost:8080//rent-a-cat");
-    // 2 | assertElementPresent | xpath=//button[text()='Rent'] | 
+    // 2 | assertElementPresent | xpath=//button[text()='Rent'] |
     {
       List<WebElement> elements = driver.findElements(By.xpath("//button[text()=\'Rent\']"));
-      assert(elements.size() > 0);
+      assert (elements.size() > 0);
     }
-    // 3 | assertElementPresent | xpath=//button[text()='Return'] | 
+    // 3 | assertElementPresent | xpath=//button[text()='Return'] |
     {
       List<WebElement> elements = driver.findElements(By.xpath("//button[text()=\'Return\']"));
-      assert(elements.size() > 0);
+      assert (elements.size() > 0);
     }
   }
+
   @Test
   public void tEST6RENT() {
     // Test name: TEST-6-RENT
     // Step # | name | target | value
-    // 1 | open | https://cs1632.appspot.com/ | 
+    // 1 | open | https://cs1632.appspot.com/ |
     driver.get("http://localhost:8080/");
-    // 2 | runScript | document.cookie = "1=false";document.cookie = "2=false";document.cookie = "3=false"; | 
+    // 2 | runScript | document.cookie = "1=false";document.cookie =
+    // "2=false";document.cookie = "3=false"; |
     js.executeScript("document.cookie = \"1=false\";document.cookie = \"2=false\";document.cookie = \"3=false\";");
-    // 3 | click | linkText=Rent-A-Cat | 
+    // 3 | click | linkText=Rent-A-Cat |
     driver.findElement(By.linkText("Rent-A-Cat")).click();
-    // 4 | click | id=rentID | 
+    // 4 | click | id=rentID |
     driver.findElement(By.id("rentID")).click();
     // 5 | type | id=rentID | 1
     driver.findElement(By.id("rentID")).sendKeys("1");
-    // 6 | click | css=.form-group:nth-child(3) .btn | 
+    // 6 | click | css=.form-group:nth-child(3) .btn |
     driver.findElement(By.cssSelector(".form-group:nth-child(3) .btn")).click();
     // 7 | assertText | xpath=//div[@id='listing']/ul/li | Rented out
     assertThat(driver.findElement(By.xpath("//div[@id=\'listing\']/ul/li")).getText(), is("Rented out"));
@@ -122,19 +131,20 @@ public class D3Test {
     // 10 | assertText | id=rentResult | Success!
     assertThat(driver.findElement(By.id("rentResult")).getText(), is("Success!"));
   }
+
   @Test
   public void tEST7RETURN() {
     // Test name: TEST-7-RETURN
     // Step # | name | target | value
-    // 1 | open | /rent-a-cat | 
+    // 1 | open | /rent-a-cat |
     driver.get("http://localhost:8080//rent-a-cat");
     // 2 | type | id=rentID | 1
     driver.findElement(By.id("rentID")).sendKeys("1");
     // 3 | type | id=returnID | 1
     driver.findElement(By.id("returnID")).sendKeys("1");
-    // 4 | click | xpath=//button[text()='Rent'] | 
+    // 4 | click | xpath=//button[text()='Rent'] |
     driver.findElement(By.xpath("//button[text()=\'Rent\']")).click();
-    // 5 | click | xpath=//button[text()='Return'] | 
+    // 5 | click | xpath=//button[text()='Return'] |
     driver.findElement(By.xpath("//button[text()=\'Return\']")).click();
     // 6 | assertText | id=cat-id1 | ID 1. Jennyanydots
     assertThat(driver.findElement(By.id("cat-id1")).getText(), is("ID 1. Jennyanydots"));
@@ -145,60 +155,67 @@ public class D3Test {
     // 9 | assertText | id=returnResult | Success!
     assertThat(driver.findElement(By.id("returnResult")).getText(), is("Success!"));
   }
+
   @Test
   public void tEST8FEEDACAT() {
     // Test name: TEST-8-FEED-A-CAT
     // Step # | name | target | value
-    // 1 | open | /feed-a-cat | 
+    // 1 | open | /feed-a-cat |
     driver.get("http://localhost:8080//feed-a-cat");
-    // 2 | assertElementPresent | xpath=//button[text()="Feed"] | 
+    // 2 | assertElementPresent | xpath=//button[text()="Feed"] |
     {
       List<WebElement> elements = driver.findElements(By.xpath("//button[text()=\"Feed\"]"));
-      assert(elements.size() > 0);
+      assert (elements.size() > 0);
     }
   }
+
   @Test
   public void tEST9FEED() {
     // Test name: TEST-9-FEED
     // Step # | name | target | value
-    // 1 | open | /feed-a-cat | 
+    // 1 | open | /feed-a-cat |
     driver.get("http://localhost:8080//feed-a-cat");
     // 2 | type | id=catnips | 6
     driver.findElement(By.id("catnips")).sendKeys("6");
-    // 3 | click | xpath=//button[text()="Feed"] | 
+    // 3 | click | xpath=//button[text()="Feed"] |
     driver.findElement(By.xpath("//button[text()=\"Feed\"]")).click();
-    // 4 | assertText | xpath=//*[@id='feedResult' and text()='Nom, nom, nom.'] | Nom, nom, nom.
-    assertThat(driver.findElement(By.xpath("//*[@id=\'feedResult\' and text()=\'Nom, nom, nom.\']")).getText(), is("Nom, nom, nom."));
+    // 4 | assertText | xpath=//*[@id='feedResult' and text()='Nom, nom, nom.'] |
+    // Nom, nom, nom.
+    assertThat(driver.findElement(By.xpath("//*[@id=\'feedResult\' and text()=\'Nom, nom, nom.\']")).getText(),
+        is("Nom, nom, nom."));
   }
+
   @Test
   public void tEST10GREETACAT() {
     // Test name: TEST-10-GREET-A-CAT
     // Step # | name | target | value
-    // 1 | open | /greet-a-cat | 
+    // 1 | open | /greet-a-cat |
     driver.get("http://localhost:8080//greet-a-cat");
     // 2 | assertText | id=greeting | Meow!Meow!Meow!
     assertThat(driver.findElement(By.id("greeting")).getText(), is("Meow!Meow!Meow!"));
   }
+
   @Test
   public void tEST11GREETACATWITHNAME() {
     // Test name: TEST-11-GREET-A-CAT-WITH-NAME
     // Step # | name | target | value
-    // 1 | open | /greet-a-cat/Jennyanydots | 
+    // 1 | open | /greet-a-cat/Jennyanydots |
     driver.get("http://localhost:8080//greet-a-cat/Jennyanydots");
     // 2 | assertText | id=greeting | Meow! from Jennyanydots.
     assertThat(driver.findElement(By.id("greeting")).getText(), is("Meow! from Jennyanydots."));
   }
+
   @Test
   public void tEST4LISTING() {
     // Test name: TEST-4-LISTING
     // Step # | name | target | value
-    // 1 | open | https://cs1632.appspot.com/ | 
+    // 1 | open | https://cs1632.appspot.com/ |
     driver.get("http://localhost:8080/");
-    // 2 | click | linkText=Catalog | 
+    // 2 | click | linkText=Catalog |
     driver.findElement(By.linkText("Catalog")).click();
     // 3 | storeXpathCount | xpath=//div/ul/li | list
     vars.put("list", driver.findElements(By.xpath("//div/ul/li")).size());
-    // 4 | echo | list | 
+    // 4 | echo | list |
     System.out.println("list");
     // 5 | assert | list | 3
     assertEquals(vars.get("list").toString(), "3");
